962295f42f82cffcb21588c31f79bd379daaab3e
tatatataiga <coffeesukidesu-1015@softbank.ne.jp>
2017-07-16 08:00:27 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
   end
-
+#=> home に対応するビュー(app/views/コントローラー名/アクション名.html.rb)を表示する
   def help
+  end
+  def about
+  end
+  def content
   end
 end
