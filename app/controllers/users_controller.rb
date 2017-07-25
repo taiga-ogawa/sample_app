cd0afaf1cfca83d60f6a68ed987af7f4f6286abf
tatatataiga <coffeesukidesu-1015@softbank.ne.jp>
2017-07-25 07:12:23 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+     def show
+          @user = User.find(params[:id])
+     end
      def new
-  end
+          @user = User.new
+     end
+     
+     def create
+          @user = User.new(user_params)
+          if @user.save
+               flash[:success] = "Welcome to  the Sample APP"
+               redirect_to @user
+               #success
+          else
+               #failure
+          render "new"
+          end
+     end
+     
+     def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+     end
 end
